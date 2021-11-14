f03034aaa172f788d033bbf3e6db1626a0780c54
David Brais <2095059@cshawi.ca>
2021-11-14 15:05:36 -0500

    working with mockUp data

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from "./hero"
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES)
+    this.messageService.add('HeroService: fetched Heroes')
+    return heroes;
+  }
+  getHero(id: Number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`heroService: fetched hero id=${id}`)
+    return of(hero);
+  }
+}
